ab93e449d3833bc13b4f96f2efb8391a40ec44f9
Michal <michalpajaczkowski@gmail.com>
2019-11-06 14:56:22 +0100

    ResultSet i insert

--- src/main/java/SQLconnection.java
+++ src/main/java/SQLconnection.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLconnection {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -9,5 +7,32 @@ public class SQLconnection {
 
         Connection db = DriverManager.getConnection(url);
         System.out.println(db);
+
+        Statement statement = db.createStatement();
+        String sqlQuery = "Select * from Employees";
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        while (rs.next()) {
+//            System.out.println(rs.getDate("StartJobDate"));
+            System.out.println(rs.getString("City"));
+        }
+        System.out.println("////////////////////");
+        //insert
+        String insert = "Insert INTO Employees (LastName, FirstName, Address, City, Salary, Age, StartJobDate, Benefit) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement ps = db.prepareStatement(insert);
+        ps.setString(1, "Jarek");
+        ps.setString(2, "Garek");
+        ps.setString(3, "Bursztynowa");
+        ps.setString(4, "Wilno");
+        ps.setInt(5, 4500);
+        ps.setInt(6, 42);
+        ps.setDate(7, Date.valueOf("2018-10-15"));
+        ps.setInt(8, 1);
+        int rowInsert = ps.executeUpdate();
+        if(rowInsert > 0) {
+            System.out.println("Success!");
+        }
+
+
     }
 }
